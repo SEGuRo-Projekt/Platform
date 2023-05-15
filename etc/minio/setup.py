658e30d092bd7756f02a5d8076b00375b55c3fff
Steffen Vogel <post@steffenvogel.de>
2023-05-16 00:38:17 +0200

    Use Python script to setup minio

---
+++ etc/minio/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import os
+import logging
+import minio
+from pprint import pformat
+
+def main():
+    logging.basicConfig(format='%(asctime)s %(name)s %(levelname)s: %(message)s', level=logging.INFO)
+
+    minio_user = os.environ.get('MINIO_ROOT_USER')
+    minio_pass = os.environ.get('MINIO_ROOT_PASSWORD')
+    minio_endpoint = 'minio:9000'
+    minio_url = f'http://{minio_endpoint}'
+
+    logging.info('Create host alias')
+    os.system(f"mc config host add minio {minio_url} {minio_user} {minio_pass}")
+
+    mc = minio.Minio(minio_endpoint, minio_user, minio_pass, secure=False)
+    mca = minio.MinioAdmin(target='minio')
+
+    logging.info('Server info: %s', pformat(mca.info()))
+
+    if mc.bucket_exists('seguro'):
+        logging.info('Bucket already exists')
+    else:
+        logging.info('Creating bucket:')
+        mc.make_bucket('seguro')
+
+if __name__ == '__main__':
+    main()
--- etc/minio/setup.sh
+++
@@ -1,7 +0,0 @@
-/usr/bin/env sh
-
-set -e
-
-mc config host add minio http://minio:9000 ${S3_USER} ${S3_PASSWORD};
-mc mb minio/seguro;
-exit 0;
