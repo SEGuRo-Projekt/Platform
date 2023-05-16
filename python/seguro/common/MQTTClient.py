a862c00d7a4bcf746bdc57bb3741f0ae9737d286
Felix Wege <fwege@eonerc.rwth-aachen.de>
2023-05-16 17:16:18 +0200

    Fix formatting of SPDX Identifiers

--- python/seguro/common/MQTTClient.py
+++ python/seguro/common/MQTTClient.py
@@ -1,5 +1,7 @@
-# SPDX-FileCopyrightText: 2023 Felix Wege, EONERC-ACS, RWTH Aachen University
-# SPDX-License-Identifier: Apache-2.0
+"""
+SPDX-FileCopyrightText: 2023 Felix Wege, EONERC-ACS, RWTH Aachen University
+SPDX-License-Identifier: Apache-2.0
+"""
 
 import paho.mqtt.client as mqtt
 from queue import Queue
--- python/seguro/common/S3Storage.py
+++ python/seguro/common/S3Storage.py
@@ -1,5 +1,7 @@
-# SPDX-FileCopyrightText: 2023 Felix Wege, EONERC-ACS, RWTH Aachen University
-# SPDX-License-Identifier: Apache-2.0
+"""
+SPDX-FileCopyrightText: 2023 Felix Wege, EONERC-ACS, RWTH Aachen University
+SPDX-License-Identifier: Apache-2.0
+"""
 
 from minio import Minio
 import io
