37534974e04697be5e56a348adca185a325cad82
Felix Wege <fwege@eonerc.rwth-aachen.de>
2023-05-16 16:44:59 +0200

    Add docstrings and spdx identifiers + fix style

--- python/seguro/common/MQTTClient.py
+++ python/seguro/common/MQTTClient.py
@@ -1,18 +1,28 @@
+# SPDX-FileCopyrightText: 2023 Felix Wege, EONERC-ACS, RWTH Aachen University
+# SPDX-License-Identifier: Apache-2.0
+
 import paho.mqtt.client as mqtt
 from queue import Queue
 
 
 class MQTTClient:
+    """Helper class for MQTT interaction with the SEGuRo platform.
+
+        This class provides an abstraction layer for MQTT based communication
+        between the SEGuRo platform and the MQTT broker.
+    """
 
     def __init__(self):
+        """MQTTClient Constructor
+
+            Create a paho.mqtt.client object and initialize the message queue.
+        """
         self.client = self.__mqtt_create_client()
         self.messageQueue = Queue()
 
-    # The callback function of connection
     def __on_connect(self, client, userdata, flags, rc):
         print(f"Connected with result code {rc}")
 
-    # The callback function for received message
     def __on_message(self, client, userdata, msg):
         print(msg.topic+" "+str(msg.payload))
         self.messageQueue.put(msg)
@@ -23,20 +33,35 @@ class MQTTClient:
         client.on_message = self.__on_message
         return client
 
-    def connect(self, broker, port, username=None, password=None, keepalive=60):
-        if username is not None or password is not None:
-            self.client.username_pw_set(username, password)
+    def connect(self, broker, port, username=None, passwd=None, keepalive=60):
+        """Connect MQTT client to broker.
+
+        Arguments:
+            broker  -- Hostname of IP address of the MQTT broker
+            port    -- Network port of the MQTT broker
+
+        Keyword arguments:
+            username    -- Username for broker authentication (default None)
+            passwd      -- Password for broker authentication (default None)
+            keepalive   -- Set keepalive interval of connection in s (default 60)
+        """
+        if username is not None or passwd is not None:
+            self.client.username_pw_set(username, passwd)
 
         self.client.connect(broker, port, keepalive)
 
     def subscribe(self, topic):
+        """Subscribe client to given topic."""
         self.client.subscribe(topic)
 
     def start_listening(self):
+        """Start async listening on subscribed topics."""
         self.client.loop_start()
 
     def stop_listening(self):
+        """Stop async listening on subscribed topics."""
         self.client.loop_stop()
 
     def publish(self, topic, message):
+        """Publish message to given topic."""
         self.client.publish(topic, message)
--- python/seguro/common/S3Storage.py
+++ python/seguro/common/S3Storage.py
@@ -1,21 +1,50 @@
+# SPDX-FileCopyrightText: 2023 Felix Wege, EONERC-ACS, RWTH Aachen University
+# SPDX-License-Identifier: Apache-2.0
+
 from minio import Minio
 import io
 
 
 class S3Storage:
+    """Helper class for S3Storage interaction with the SEGuRo platform
+
+        This class provides an abstraction layer for interactions btween
+        the S3Storage and the SEGuRo platform.
+    """
 
     def __init__(self, server, port, access_key, secret_key):
+        """S3Storage Constructor
+
+            Connect to an S3Storage using the given credentials and store the
+            returned client handle.
+
+            Arguments:
+                server      -- Hostname or IP address of the storage server
+                port        -- Network port of the storage server
+                access_key  -- Access key (UID) for authentication
+                secret_key  -- Secret key (password) for authentication
+
+        """
         self.tracked_files = {}
         self.client = self.__connect(server, port, access_key, secret_key)
 
     def __connect(self, server, port, access_key, secret_key):
         client = Minio(endpoint=server+":"+str(port),
-                    access_key=access_key,
-                    secret_key=secret_key,
-                    secure=False)
+                       access_key=access_key,
+                       secret_key=secret_key,
+                       secure=False)
         return client
 
     def get_file(self, filename, file, bucket="seguro"):
+        """Download file from the S3Storage and store it locally.
+
+        Arguments:
+            filename -- Local filename that is used
+            file     -- Name of requested file in storage
+
+        Keyword arguments:
+            bucket -- Storage bucket that is considered (default "seguro")
+        """
         if not self.client.bucket_exists(bucket):
             print(f"Error: Bucket \"{bucket}\" does not exist...")
             return
@@ -25,6 +54,15 @@ class S3Storage:
         )
 
     def put_file(self, filename, file, bucket="seguro"):
+        """Upload local file and store it in the S3Storage.
+
+        Arguments:
+            filename -- Name of uploaded file in storage
+            file     -- Local file that is used
+
+        Keyword arguments:
+            bucket -- Storage bucket that is considered (default "seguro")
+        """
         if not self.client.bucket_exists(bucket):
             print(f"Error: Bucket \"{bucket}\" does not exist...")
             return
@@ -33,16 +71,39 @@ class S3Storage:
             bucket, filename, file
         )
 
-    def write_to_file(self, filename, message, bucket="seguro"):
+    def write_to_file(self, filename, content, bucket="seguro"):
+        """Write to file stored it in the S3Storage.
+
+        Arguments:
+            filename -- Name of file in storage
+            content  -- Content that is written to file
+
+        Keyword arguments:
+            bucket -- Storage bucket that is considered (default "seguro")
+        """
         if not self.client.bucket_exists(bucket):
             print(f"Error: Bucket \"{bucket}\" does not exist...")
             return
 
         self.client.put_object(
-            bucket, filename, io.BytesIO(b'%b' % message.encode('utf8')), len(message)
+            bucket,
+            filename,
+            io.BytesIO(b'%b' % content.encode('utf8')), len(content)
         )
 
     def file_changed(self, filename, bucket="seguro"):
+        """Check if file has changed since last call.
+
+            Arguments:
+                filename -- Local filename that is used
+
+            Keyword arguments:
+                bucket -- Storage bucket that is considered (default "seguro")
+
+            Returns:
+                True  -- If file has changed or if file untracked
+                False -- Else
+        """
         if not self.client.bucket_exists(bucket):
             print(f"Error: Bucket \"{bucket}\" does not exist...")
             return
@@ -55,13 +116,13 @@ class S3Storage:
             self.tracked_files[filename] = stats._last_modified
             # if file is not tracked yet, assume it has changed
             return True
-        else:
-            if self.tracked_files[filename] != stats._last_modified:
-                print(self.tracked_files[filename])
-                print(stats._last_modified)
-                self.tracked_files[filename] = stats._last_modified
-                return True
-            else:
-                print(self.tracked_files[filename])
-                print(stats._last_modified)
-                return False
+
+        if self.tracked_files[filename] != stats._last_modified:
+            print(self.tracked_files[filename])
+            print(stats._last_modified)
+            self.tracked_files[filename] = stats._last_modified
+            return True
+
+        print(self.tracked_files[filename])
+        print(stats._last_modified)
+        return False
