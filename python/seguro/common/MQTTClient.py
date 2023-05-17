222fcebcb2b8b9531ae7be3e774cac7ba126f9a8
Felix Wege <fwege@eonerc.rwth-aachen.de>
2023-05-17 15:46:33 +0200

    MQTTClient construct allows setting client uid

--- python/seguro/common/MQTTClient.py
+++ python/seguro/common/MQTTClient.py
@@ -3,34 +3,38 @@ SPDX-FileCopyrightText: 2023 Felix Wege, EONERC-ACS, RWTH Aachen University
 SPDX-License-Identifier: Apache-2.0
 """
 
-import paho.mqtt.client as mqtt
 from queue import Queue
+
+import paho.mqtt.client as mqtt
 
 
 class MQTTClient:
     """Helper class for MQTT interaction with the SEGuRo platform.
 
-        This class provides an abstraction layer for MQTT based communication
-        between the SEGuRo platform and the MQTT broker.
+    This class provides an abstraction layer for MQTT based communication
+    between the SEGuRo platform and the MQTT broker.
     """
 
-    def __init__(self):
+    def __init__(self, uid=""):
         """MQTTClient Constructor
 
-            Create a paho.mqtt.client object and initialize the message queue.
+        Create a paho.mqtt.client object and initialize the message queue.
+
+        Arguments:
+            uid -- Unique id/name of the client (default empty string "")
         """
-        self.client = self.__mqtt_create_client()
+        self.client = self.__mqtt_create_client(uid)
         self.message_queue = Queue()
 
     def __on_connect(self, client, userdata, flags, rc):
         print(f"Connected with result code {rc}")
 
     def __on_message(self, client, userdata, msg):
-        print(msg.topic+" "+str(msg.payload))
+        print(msg.topic + " " + str(msg.payload))
         self.message_queue.put(msg)
 
-    def __mqtt_create_client(self):
-        client = mqtt.Client()
+    def __mqtt_create_client(self, uid):
+        client = mqtt.Client(client_id=uid)
         client.on_connect = self.__on_connect
         client.on_message = self.__on_message
         return client
@@ -43,13 +47,12 @@ class MQTTClient:
             port    -- Network port of the MQTT broker
 
         Keyword arguments:
-            username    -- Username for broker authentication (default None)
-            passwd      -- Password for broker authentication (default None)
-            keepalive   -- Set keepalive interval of connection in s (default 60)
+            username   -- Username for broker authentication (default None)
+            passwd     -- Password for broker authentication (default None)
+            keepalive  -- Set keepalive interval of connection (default 60s)
         """
         if username is not None or passwd is not None:
             self.client.username_pw_set(username, passwd)
-
         self.client.connect(broker, port, keepalive)
 
     def subscribe(self, topic):
