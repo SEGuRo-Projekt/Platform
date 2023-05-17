924668cbb7651d5507a0b9d0673c6ad68c7eb7d7
Felix Wege <fwege@eonerc.rwth-aachen.de>
2023-05-17 16:02:21 +0200

    Fix pipeline

--- python/seguro/common/__init__.py
+++ python/seguro/common/__init__.py
@@ -2,6 +2,3 @@
 SPDX-FileCopyrightText: 2023 Felix Wege, EONERC-ACS, RWTH Aachen University
 SPDX-License-Identifier: Apache-2.0
 """
-
-from .S3Storage import S3Storage
-from .MQTTClient import MQTTClient
