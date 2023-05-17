786682decf236bb135a6f6a00bdeb19f2eac4abf
Felix Wege <fwege@eonerc.rwth-aachen.de>
2023-05-17 16:02:21 +0200

    Fix pipeline

--- python/seguro/common/S3Storage.py
+++ python/seguro/common/S3Storage.py
@@ -3,38 +3,40 @@ SPDX-FileCopyrightText: 2023 Felix Wege, EONERC-ACS, RWTH Aachen University
 SPDX-License-Identifier: Apache-2.0
 """
 
-from minio import Minio
 import io
+
+from minio import Minio
 
 
 class S3Storage:
     """Helper class for S3Storage interaction with the SEGuRo platform
 
-        This class provides an abstraction layer for interactions btween
-        the S3Storage and the SEGuRo platform.
+    This class provides an abstraction layer for interactions btween
+    the S3Storage and the SEGuRo platform.
     """
 
     def __init__(self, server, port, access_key, secret_key):
         """S3Storage Constructor
 
-            Connect to an S3Storage using the given credentials and store the
-            returned client handle.
+        Connect to an S3Storage using the given credentials and store the
+        returned client handle.
 
-            Arguments:
-                server      -- Hostname or IP address of the storage server
-                port        -- Network port of the storage server
-                access_key  -- Access key (UID) for authentication
-                secret_key  -- Secret key (password) for authentication
-
+        Arguments:
+            server      -- Hostname or IP address of the storage server
+            port        -- Network port of the storage server
+            access_key  -- Access key (UID) for authentication
+            secret_key  -- Secret key (password) for authentication
         """
         self.tracked_files = {}
         self.client = self.__connect(server, port, access_key, secret_key)
 
     def __connect(self, server, port, access_key, secret_key):
-        client = Minio(endpoint=server+":"+str(port),
-                       access_key=access_key,
-                       secret_key=secret_key,
-                       secure=False)
+        client = Minio(
+            endpoint=server + ":" + str(port),
+            access_key=access_key,
+            secret_key=secret_key,
+            secure=False,
+        )
         return client
 
     def get_file(self, filename, file, bucket="seguro"):
@@ -48,12 +50,10 @@ class S3Storage:
             bucket -- Storage bucket that is considered (default "seguro")
         """
         if not self.client.bucket_exists(bucket):
-            print(f"Error: Bucket \"{bucket}\" does not exist...")
+            print(f"Error: Bucket {bucket} does not exist...")
             return
 
-        self.client.fget_object(
-            bucket, file, filename
-        )
+        self.client.fget_object(bucket, file, filename)
 
     def put_file(self, filename, file, bucket="seguro"):
         """Upload local file and store it in the S3Storage.
@@ -66,12 +66,10 @@ class S3Storage:
             bucket -- Storage bucket that is considered (default "seguro")
         """
         if not self.client.bucket_exists(bucket):
-            print(f"Error: Bucket \"{bucket}\" does not exist...")
+            print(f"Error: Bucket {bucket} does not exist...")
             return
 
-        self.client.fput_object(
-            bucket, filename, file
-        )
+        self.client.fput_object(bucket, filename, file)
 
     def write_to_file(self, filename, content, bucket="seguro"):
         """Write to file stored it in the S3Storage.
@@ -84,35 +82,34 @@ class S3Storage:
             bucket -- Storage bucket that is considered (default "seguro")
         """
         if not self.client.bucket_exists(bucket):
-            print(f"Error: Bucket \"{bucket}\" does not exist...")
+            print(f"Error: Bucket {bucket} does not exist...")
             return
 
         self.client.put_object(
             bucket,
             filename,
-            io.BytesIO(b'%b' % content.encode('utf8')), len(content)
+            io.BytesIO(b"%b" % content.encode("utf8")),
+            len(content),
         )
 
     def file_changed(self, filename, bucket="seguro"):
         """Check if file has changed since last call.
 
-            Arguments:
-                filename -- Local filename that is used
+        Arguments:
+            filename -- Local filename that is used
 
-            Keyword arguments:
-                bucket -- Storage bucket that is considered (default "seguro")
+        Keyword arguments:
+            bucket -- Storage bucket that is considered (default "seguro")
 
-            Returns:
-                True  -- If file has changed or if file untracked
-                False -- Else
+        Returns:
+            True  -- If file has changed or if file untracked
+            False -- Else
         """
         if not self.client.bucket_exists(bucket):
-            print(f"Error: Bucket \"{bucket}\" does not exist...")
+            print(f"Error: Bucket {bucket} does not exist...")
             return
 
-        stats = self.client.stat_object(
-            bucket, filename
-        )
+        stats = self.client.stat_object(bucket, filename)
 
         if filename not in self.tracked_files.keys():
             self.tracked_files[filename] = stats._last_modified
