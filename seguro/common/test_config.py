c45d6ee56dd7675de6d91b54d0e91927cbf55148
Steffen Vogel <steffen.vogel@opal-rt.com>
2023-05-26 16:32:44 +0200

    Fix config test

--- seguro/common/test_config.py
+++ seguro/common/test_config.py
@@ -4,8 +4,8 @@ SPDX-License-Identifier: Apache-2.0
 """
 
 
-from seguro.common.config import S3_HOST
+from seguro.common.config import S3_BUCKET
 
 
 def test_default_config():
-    assert S3_HOST == "minio"
+    assert S3_BUCKET == "seguro"
