69082429754ff022fe849e8e6bb45173ccb8898b
Steffen Vogel <steffen.vogel@opal-rt.com>
2023-05-26 16:31:32 +0200

    Make S3 bucket configurable

--- seguro/common/test_store.py
+++ seguro/common/test_store.py
@@ -21,32 +21,24 @@ def test_store():
     f.close()
 
     # Put file into storage
-    storage.put_file("myStorageFile.txt", "myfile.txt", bucket="testbucket")
+    storage.put_file("myStorageFile.txt", "myfile.txt")
 
     # Check if file has changed...
     # Note: As the storage client cannot know if the file changed on
     #       the first call file_changed() will always return True on first call
-    assert (
-        storage.file_changed("myStorageFile.txt", bucket="testbucket") is True
-    )
-    assert (
-        storage.file_changed("myStorageFile.txt", bucket="testbucket") is False
-    )
+    assert storage.file_changed("myStorageFile.txt") is True
+    assert storage.file_changed("myStorageFile.txt") is False
 
     # Write new content to file
     # FIXME: last_modified attribute of S3Storage only retruns
     #        second resolution...
     time.sleep(1)
-    storage.write_to_file(
-        "myStorageFile.txt", "!egarotS3S olleH", bucket="testbucket"
-    )
+    storage.write_to_file("myStorageFile.txt", "!egarotS3S olleH")
 
     # If file has changed (which it should at this point), download it...
-    if storage.file_changed("myStorageFile.txt", bucket="testbucket"):
+    if storage.file_changed("myStorageFile.txt"):
         print("Info: myStorageFile.txt has changed - downloading it again...")
-        storage.get_file(
-            "myLocalStorageFile.txt", "myStorageFile.txt", bucket="testbucket"
-        )
+        storage.get_file("myLocalStorageFile.txt", "myStorageFile.txt")
 
     f = open("myLocalStorageFile.txt", "r")
     content = f.read()
