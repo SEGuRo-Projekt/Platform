d40c80b5d5cf8e08c396dce4674afa6aedfdd290
Steffen Vogel <steffen.vogel@opal-rt.com>
2023-05-30 23:11:33 +0200

    Add watch sync/async watch functions to store client

--- seguro/common/store.py
+++ seguro/common/store.py
@@ -4,6 +4,8 @@ SPDX-License-Identifier: Apache-2.0
 """
 
 import io
+import threading
+import enum
 
 from minio import Minio
 
@@ -18,7 +20,32 @@ from seguro.common.config import (
 )
 
 
-class StoreClient:
+class EventType(enum.Flag):
+    CREATED = enum.auto()
+    REMOVED = enum.auto()
+
+
+class Event:
+    def __init__(self, event):
+        records = event.get("Records")
+        record = records[0]
+
+        event_name: str = record.get("eventName")
+        s3: dict = record.get("s3", {})
+        obj: str = s3.get("object", {})
+
+        self.filename: str = obj.get("key")
+
+        if event_name.startswith("s3:ObjectCreated"):
+            self.type = EventType.CREATED
+        elif event_name.startswith("s3:ObjectRemoved"):
+            self.type = EventType.REMOVED
+
+    def __str__(self) -> str:
+        return f"{self.filename} {self.type.name}"
+
+
+class Client:
     """Helper class for S3 object store interaction with the SEGuRo platform
 
     This class provides an abstraction layer for interactions between
@@ -33,6 +60,7 @@ class StoreClient:
         secret_key=S3_SECRET_KEY,
         secure=S3_SECURE,
         region=S3_REGION,
+        bucket=S3_BUCKET,
     ):
         """Store Constructor
 
@@ -45,7 +73,8 @@ class StoreClient:
             access_key  -- Access key (UID) for authentication
             secret_key  -- Secret key (password) for authentication
         """
-        self.tracked_files = {}
+        self.bucket = bucket
+
         self.client = Minio(
             endpoint=f"{host}:{port}",
             access_key=access_key,
@@ -54,89 +83,88 @@ class StoreClient:
             region=region,
         )
 
-    def get_file(self, filename, file, bucket=S3_BUCKET):
+        if not self.client.bucket_exists(self.bucket):
+            raise Exception(f"Error: Bucket {self.bucket} does not exist...")
+
+    def get_file(self, filename, file):
         """Download file from the S3 object store and store it locally.
 
         Arguments:
             filename -- Local filename that is used
             file     -- Name of requested file in storage
-
-        Keyword arguments:
-            bucket -- Storage bucket that is considered (default "seguro")
         """
-        if not self.client.bucket_exists(bucket):
-            print(f"Error: Bucket {bucket} does not exist...")
-            return
+        return self.client.fget_object(self.bucket, file, filename)
 
-        self.client.fget_object(bucket, file, filename)
-
-    def put_file(self, filename, file, bucket=S3_BUCKET):
+    def put_file(self, filename, file):
         """Upload local file and store it in the S3Storage.
 
         Arguments:
             filename -- Name of uploaded file in storage
             file     -- Local file that is used
-
-        Keyword arguments:
-            bucket -- Storage bucket that is considered (default "seguro")
         """
-        if not self.client.bucket_exists(bucket):
-            print(f"Error: Bucket {bucket} does not exist...")
-            return
+        return self.client.fput_object(self.bucket, filename, file)
 
-        self.client.fput_object(bucket, filename, file)
+    def remove_file(self, filename):
+        return self.client.remove_object(self.bucket, filename)
 
-    def write_to_file(self, filename, content, bucket=S3_BUCKET):
+    def put_file_contents(self, filename, content):
         """Write to file stored it in the S3Storage.
 
         Arguments:
             filename -- Name of file in storage
             content  -- Content that is written to file
-
-        Keyword arguments:
-            bucket -- Storage bucket that is considered (default "seguro")
         """
-        if not self.client.bucket_exists(bucket):
-            print(f"Error: Bucket {bucket} does not exist...")
-            return
 
-        self.client.put_object(
-            bucket,
+        return self.client.put_object(
+            self.bucket,
             filename,
             io.BytesIO(b"%b" % content.encode("utf8")),
             len(content),
         )
 
-    def file_changed(self, filename, bucket=S3_BUCKET):
-        """Check if file has changed since last call.
+    def watch(self, prefix: str, events=EventType.CREATED | EventType.REMOVED):
+        s3_events = []
+        if EventType.CREATED in events:
+            s3_events.append("s3:ObjectCreated:*")
+        if EventType.REMOVED in events:
+            s3_events.append("s3:ObjectRemoved:*")
 
-        Arguments:
-            filename -- Local filename that is used
+        with self.client.listen_bucket_notification(
+            self.bucket,
+            prefix=prefix,
+            events=s3_events,
+        ) as events:
+            for event in events:
+                yield Event(event)
 
-        Keyword arguments:
-            bucket -- Storage bucket that is considered (default "seguro")
+    def watch_async(
+        self,
+        prefix: str,
+        cb: callable,
+        events=EventType.CREATED | EventType.REMOVED,
+    ):
+        return Watcher(self, prefix, cb, events)
 
-        Returns:
-            True  -- If file has changed or if file untracked
-            False -- Else
-        """
-        if not self.client.bucket_exists(bucket):
-            print(f"Error: Bucket {bucket} does not exist...")
-            return
 
-        stats = self.client.stat_object(bucket, filename)
+class Watcher(threading.Thread):
+    def __init__(
+        self, client: Client, prefix: str, cb: callable, events: EventType
+    ):
+        super().__init__()
 
-        if filename not in self.tracked_files.keys():
-            self.tracked_files[filename] = stats._last_modified
-            # if file is not tracked yet, assume it has changed
-            return True
+        self.events = client.watch(prefix, events)
+        self.cb = cb
+        self._stopflag = threading.Event()
 
-        if self.tracked_files[filename] != stats._last_modified:
-            print(self.tracked_files[filename])
-            print(stats._last_modified)
-            self.tracked_files[filename] = stats._last_modified
-            return True
+    def run(self):
+        for event in self.events:
+            if self._stopflag.is_set():
+                break
 
-        print(self.tracked_files[filename])
-        print(stats._last_modified)
-        return False
+            try:
+                self.cb(event)
+            except StopIteration:
+                break
+
+    def stop(self):
+        self._stopflag.set()
--- seguro/common/test_store.py
+++ seguro/common/test_store.py
@@ -4,13 +4,13 @@ SPDX-License-Identifier: Apache-2.0
 """
 
 import os
-import time
+import threading
 
-from seguro.common.store import StoreClient
+from seguro.common.store import Client, Event, EventType
 
 
 def test_store():
-    storage = StoreClient()
+    store = Client()
 
     # Create new file and fill with content
     if not os.path.isfile("myfile.txt"):
@@ -21,27 +21,73 @@ def test_store():
     f.close()
 
     # Put file into storage
-    storage.put_file("myStorageFile.txt", "myfile.txt")
-
-    # Check if file has changed...
-    # Note: As the storage client cannot know if the file changed on
-    #       the first call file_changed() will always return True on first call
-    assert storage.file_changed("myStorageFile.txt") is True
-    assert storage.file_changed("myStorageFile.txt") is False
+    store.put_file("myStorageFile.txt", "myfile.txt")
 
     # Write new content to file
-    # FIXME: last_modified attribute of S3Storage only retruns
-    #        second resolution...
-    time.sleep(1)
-    storage.write_to_file("myStorageFile.txt", "!egarotS3S olleH")
+    store.put_file_contents("myStorageFile.txt", "!egarotS3S olleH")
 
-    # If file has changed (which it should at this point), download it...
-    if storage.file_changed("myStorageFile.txt"):
-        print("Info: myStorageFile.txt has changed - downloading it again...")
-        storage.get_file("myLocalStorageFile.txt", "myStorageFile.txt")
+    store.get_file("myLocalStorageFile.txt", "myStorageFile.txt")
 
     f = open("myLocalStorageFile.txt", "r")
     content = f.read()
 
     assert content == "!egarotS3S olleH"
     print(content)
+
+
+def test_watch():
+    store = Client()
+
+    filename = "some/prefix/test"
+
+    store.remove_file(filename)
+
+    def create_obj():
+        store.put_file_contents(filename, "this is a test2")
+        store.remove_file(filename)
+
+    t = threading.Timer(0.1, create_obj)
+    t.start()
+
+    events = store.watch("some/prefix/")
+
+    event = next(events)
+    assert event.type == EventType.CREATED
+    assert event.filename == filename
+
+    event = next(events)
+    assert event.type == EventType.REMOVED
+    assert event.filename == filename
+
+
+def test_watch_async():
+    store = Client()
+
+    i = 0
+
+    def callback(event: Event):
+        nonlocal i
+
+        if i == 0:
+            assert event.type == EventType.CREATED
+            assert event.filename == filename
+        elif i == 1:
+            assert event.type == EventType.REMOVED
+            assert event.filename == filename
+
+            # Raise a StopIteration exception to stop processing events
+            raise StopIteration()
+
+        i += 1
+
+    filename = "some/prefix/test"
+
+    store.remove_file(filename)
+    watcher = store.watch_async("some/prefix/", callback)
+
+    watcher.start()
+
+    store.put_file_contents(filename, "this is a test2")
+    store.remove_file(filename)
+
+    watcher.join()
