ebe352108e4628ec64bccd1c6fc9668b6ad24371
Felix Wege <fwege@eonerc.rwth-aachen.de>
2023-06-19 16:38:13 +0200

    Add testing for topic speficic callbacks + mark tests

--- seguro/common/test_broker.py
+++ seguro/common/test_broker.py
@@ -5,26 +5,37 @@ SPDX-License-Identifier: Apache-2.0
 """
 
 import time
+import pytest
 
 from seguro.common.broker import BrokerClient
 
+# Used to store callback messages
+messages = []
 
+
+def callback(client, userdata, msg):
+    messages.append((msg.topic, msg.payload))
+
+
+@pytest.mark.broker
 def test_broker():
     broker = BrokerClient("pytest-broker")
 
     # Subscribe to topic "mytopic" and start listening in another thread
     broker.subscribe("mytopic")
-    broker.start_listening()
+    broker.subscribe("myCustomTopic", callback)
+    # broker.start_listening()
 
     # Publish messages to topic "mytopic"
     broker.publish("mytopic", "Hello MQTT!")
-    broker.publish("mytopic", "Aaaand another one...")
+    broker.publish("myCustomTopic", "Aaaand another one...")
 
     #  Make sure messages are completely sent...
     time.sleep(1)
 
-    # Read messages are stored in a messageQueue of the client
+    # Read messages on "mytopic" are stored in a messageQueue of the client
     assert broker.message_queue.empty() is False
+    assert messages[0] == ("myCustomTopic", b"Aaaand another one...")
 
     # Read messages directly from client message queue
     while not broker.message_queue.empty():
@@ -33,9 +44,6 @@ def test_broker():
 
     # Note that queue.get() removes messages from queue
     assert broker.message_queue.empty() is True
-
-    # Stop listening for new messages
-    broker.stop_listening()
 
     # Note that messsages send after listening has stopped,
     # are not put into message queue
--- seguro/common/test_config.py
+++ seguro/common/test_config.py
@@ -1,11 +1,13 @@
 """
 SPDX-FileCopyrightText: 2023 Steffen Vogel, OPAL-RT Germany GmbH
+SPDX-FileCopyrightText: 2023 Felix Wege, EONERC-ACS, RWTH Aachen University
 SPDX-License-Identifier: Apache-2.0
 """
 
-
+import pytest
 from seguro.common.config import S3_BUCKET
 
 
+@pytest.mark.config
 def test_default_config():
     assert S3_BUCKET == "seguro"
