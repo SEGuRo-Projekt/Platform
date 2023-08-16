7b7ca673c4863f2941147453418719dd3ff7b92c
Steffen Vogel <steffen.vogel@opal-rt.com>
2023-08-16 14:35:24 +0000

    Add simple script to produce random test data
    Signed-off-by: Steffen Vogel <steffen.vogel@opal-rt.com>

---
+++ seguro/demo_data/main.py
@@ -0,0 +1,31 @@
+"""
+SPDX-FileCopyrightText: 2023 Steffen Vogel, OPAL-RT Germany GmbH
+SPDX-License-Identifier: Apache-2.0
+"""
+
+import time
+import random
+import logging
+
+from seguro.common.broker import BrokerClient
+
+
+def main():
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s.%(msecs)03d %(levelname)s %(message)s",
+        datefmt="%H:%M:%S",
+    )
+
+    broker = BrokerClient("demo-data")
+
+    value = 0
+
+    while True:
+        broker.publish("measurements/ms1/value", value)
+        value += 0.1 * random.uniform(-1, 1)
+        time.sleep(0.1)
+
+
+if __name__ == "__main__":
+    main()
