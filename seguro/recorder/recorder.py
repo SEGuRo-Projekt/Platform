498dc9465a1ebce9e84b7a726d3cd8717aa379e8
Steffen Vogel <steffen.vogel@opal-rt.com>
2023-08-23 17:14:41 +0200

    Add first version of recorder
    Signed-off-by: Steffen Vogel <steffen.vogel@opal-rt.com>

---
+++ seguro/recorder/recorder.py
@@ -0,0 +1,15 @@
+"""
+SPDX-FileCopyrightText: 2023 Steffen Vogel, OPAL-RT Germany GmbH
+SPDX-License-Identifier: Apache-2.0
+"""
+
+
+class Recorder:
+    def __init__(self, config):
+        pass
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
