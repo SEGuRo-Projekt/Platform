aca66530370e1a926e0c716e300847093d11ae0b
Steffen Vogel <steffen.vogel@opal-rt.com>
2024-03-27 12:51:34 +0100

    recorder: Add configless version
    Signed-off-by: Steffen Vogel <steffen.vogel@opal-rt.com>

--- seguro/recorder/main.py
+++ seguro/recorder/main.py
@@ -3,30 +3,65 @@ SPDX-FileCopyrightText: 2023 Steffen Vogel, OPAL-RT Germany GmbH
 SPDX-License-Identifier: Apache-2.0
 """
 
+import argparse
 import logging
 import time
+from typing import Iterable
+import functools as ft
 
-from seguro.common.broker import BrokerClient, Message
+from seguro.common import store, broker
+from seguro.common.broker import Sample
+from seguro.recorder.recorder import Recorder
+
+recorders: dict[str, Recorder] = {}
 
 
-def new_value(broker: BrokerClient, msg: Message):
-    value = float(msg.payload)
-    logging.info(f"Got value {value:.2}")
+def on_samples(
+    s: store.Client, _: broker.Client, topic: str, samples: Iterable[Sample]
+):
+    try:
+        recorder = recorders[topic]
+    except KeyError:
+        recorder = Recorder(s, topic)
+        recorders[topic] = recorder
+
+    recorder.record_samples(samples)
 
 
 def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-p", "--prefix", type=str, default="data")
+    parser.add_argument(
+        "-l",
+        "--log-level",
+        default="info",
+        help="Logging level",
+        choices=["debug", "info", "warn", "error", "critical"],
+    )
+
+    args = parser.parse_args()
+
     logging.basicConfig(
-        level=logging.DEBUG,
-        format="%(asctime)s.%(msecs)03d %(levelname)s %(message)s",
+        level=args.log_level.upper(),
+        format="%(asctime)s.%(msecs)03d %(levelname)s %(name)s %(message)s",
         datefmt="%H:%M:%S",
     )
 
-    broker = BrokerClient("recorder")
+    b = broker.Client("recorder")
+    s = store.Client()
 
-    broker.subscribe("measurements/ms1/value", new_value)
+    topic = f"{args.prefix}/#"
+
+    b.subscribe_samples(topic, ft.partial(on_samples, s))
+
+    logging.info("Subscribed to topic: %s", topic)
 
     while True:
-        time.sleep(1)
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            break
 
 
 if __name__ == "__main__":
--- seguro/recorder/recorder.py
+++ seguro/recorder/recorder.py
@@ -3,13 +3,74 @@ SPDX-FileCopyrightText: 2023 Steffen Vogel, OPAL-RT Germany GmbH
 SPDX-License-Identifier: Apache-2.0
 """
 
+import logging
+import math
+from typing import Iterable
+from pathlib import Path
+import pandas as pd
+
+from seguro.common import store
+from villas.node.sample import Sample
+
 
 class Recorder:
-    def __init__(self, config):
-        pass
+    def __init__(self, s: store.Client, topic: str):
+        self.path = Path(topic)
 
-    def start(self):
-        pass
+        logger_name = ".".join(["recorder"] + topic.split("/")[1:])
+        self.logger = logging.getLogger(logger_name)
 
-    def stop(self):
-        pass
+        self.store = s
+
+        self.samples: list[Sample]
+        self.max_values: int
+
+        self._reset()
+
+    def record_samples(self, samples: Iterable[Sample]):
+        self.logger.debug("Recording %d samples", len(samples))
+
+        for sample in samples:
+            if sample.new_frame:
+                self._write_frame()
+
+            self.samples.append(sample)
+            self.max_values = max(self.max_values, len(sample.data))
+
+    def _write_frame(self):
+        if len(self.samples) == 0:
+            return
+
+        self.logger.info(
+            "Writing frame object containing %d samples",
+            len(self.samples),
+        )
+
+        index = [
+            pd.Timestamp(
+                sample.ts_origin.seconds * 1000000000
+                + sample.ts_origin.nanoseconds
+            )
+            for sample in self.samples
+        ]
+        data = {"sequence": [sample.sequence for sample in self.samples]}
+
+        for i in range(self.max_values):
+            data[f"signal{i}"] = [
+                sample.data[i] if i < len(sample.data) else math.nan
+                for sample in self.samples
+            ]
+
+        df = pd.DataFrame(data, index)
+
+        ts = self.samples[0].ts_origin.datetime()
+        ts = ts.replace(microsecond=0)
+        obj_path = self.path / f"{ts.isoformat()}.parquet"
+
+        self.store.put_frame(obj_path, df)
+
+        self._reset()
+
+    def _reset(self):
+        self.samples = []
+        self.max_values = 0
