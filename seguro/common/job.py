b5e5bedbe8267a8be31917c2becaffc1d9903c2d
Steffen Vogel <steffen.vogel@opal-rt.com>
2024-04-04 13:46:25 +0200

    docs: Fix exception caused by missing env var in Sphinx autoapi imports
    Signed-off-by: Steffen Vogel <steffen.vogel@opal-rt.com>

--- seguro/common/job.py
+++ seguro/common/job.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: 2023 Steffen Vogel, OPAL-RT Germany GmbH
 # SPDX-License-Identifier: Apache-2.0
 
+import sys
 import os
 import json
 from types import SimpleNamespace
@@ -9,6 +10,9 @@ from types import SimpleNamespace
 def _get_job_info():
     info_raw = os.environ.get("SEGURO_JOB_INFO")
     if info_raw is None:
+        if "sphinx" in sys.modules:
+            return None
+
         raise Exception("Missing job details")
 
     return json.loads(
