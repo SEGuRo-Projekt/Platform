c19b8b4fce82f74118a6a86c702330a9da49d073
Steffen Vogel <steffen.vogel@opal-rt.com>
2024-04-16 10:14:50 +0200

    Generate CA and client certificates
    Signed-off-by: Steffen Vogel <steffen.vogel@opal-rt.com>

--- setup.sh
+++ setup.sh
@@ -2,29 +2,116 @@
 # SPDX-FileCopyrightText: 2023 Steffen Vogel, OPAL-RT Germany GmbH
 # SPDX-License-Identifier: Apache-2.0
 
-if ! [ -f /keys/ssh_host_rsa_key ]; then
+set -e
+
+PKI_CLIENT_CERTS="mp1 mp2 admin"
+PKI_SUBJECT="/C=DE/ST=NRW/L=Aachen/O=OPAL-RT Germany GmbH/OU=Research and Development"
+PKI_KEY_TYPE="rsa:4096"
+PKI_EXPIRY_DAYS=3650
+
+function create_ssh_keys() {
   echo "== Creating SSH host key..."
   ssh-keygen -N '' -t rsa -b 4096 -f /keys/ssh_host_rsa_key
   echo "Public host key: $(cat /keys/ssh_host_rsa_key.pub)"
   echo
-fi
+}
 
-if ! [ -f /keys/server.crt -a -f /keys/server.key ]; then
-  echo "== Creating self-signed server cert..."
+function create_pki_ca() {
+  echo "=== Creating CA certificate"
   openssl req \
-    -nodes \
     -x509 \
+    -new \
+    -nodes \
     -sha256 \
-    -newkey rsa:4096 \
-    -keyout /keys/server.key \
-    -out /keys/server.crt \
-    -days 3560 \
-    -subj "/C=DE/ST=NRW/L=Aachen/O=OPAL-RT/OU=RnD/CN=localhost" \
-    -addext "subjectAltName = DNS:*.localhost,DNS:localhost,DNS:*.seguro,DNS:seguro" > /dev/null
+    -days ${PKI_EXPIRY_DAYS} \
+    -newkey ${PKI_KEY_TYPE} \
+    -keyout /keys/ca.key \
+    -out /certs/ca.crt \
+    -subj "${PKI_SUBJECT}/CN=SEGuRo Certificate Authority" 2> /dev/null
+}
 
-  openssl x509 -in /keys/server.crt -text -noout
-  echo
+function create_pki_server() {
+  echo "=== Creating server certificate"
+  cat > server.v3.ext << EOF
+authorityKeyIdentifier = keyid, issuer
+basicConstraints = CA:FALSE
+keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
+extendedKeyUsage = serverAuth
+subjectAltName = @alt_names
+[alt_names]
+DNS.1 = ${DOMAIN}
+DNS.2 = *.${DOMAIN}
+EOF
+
+  openssl req \
+    -new \
+    -nodes \
+    -out server.csr \
+    -newkey ${PKI_KEY_TYPE} \
+    -keyout /keys/server.key \
+    -subj "${PKI_SUBJECT}/CN=Server Certificate" 2> /dev/null
+
+  openssl x509 \
+    -req \
+    -days ${PKI_EXPIRY_DAYS} \
+    -sha256 \
+    -extfile server.v3.ext \
+    -CA /certs/ca.crt \
+    -CAkey /keys/ca.key \
+    -CAcreateserial \
+    -in server.csr \
+    -out /certs/server.crt 2> /dev/null
+}
+
+function create_pki_client() {
+  CN=$1
+
+  echo "=== Creating client certificate for ${CN}"
+  cat > client.v3.ext << EOF
+authorityKeyIdentifier = keyid, issuer
+basicConstraints = CA:FALSE
+keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
+extendedKeyUsage = clientAuth
+EOF
+
+  openssl req \
+    -new \
+    -nodes \
+    -out client-${CN}.csr \
+    -newkey ${PKI_KEY_TYPE} \
+    -keyout /keys/client-${CN}.key \
+    -subj "${PKI_SUBJECT}/CN=${CN}" 2> /dev/null
+
+  openssl x509 \
+    -req \
+    -days ${PKI_EXPIRY_DAYS} \
+    -sha256 \
+    -extfile client.v3.ext \
+    -CA /certs/ca.crt \
+    -CAkey /keys/ca.key \
+    -CAcreateserial \
+    -in client-${CN}.csr \
+    -out /certs/client-${CN}.crt 2> /dev/null
+}
+
+if ! [ -f /keys/ssh_host_rsa_key ]; then
+  create_ssh_keys
 fi
+
+echo "== Checking PKI certificates..."
+[ -f /certs/ca.crt -a -f /keys/ca.key ] || \
+create_pki_ca
+
+[ -f /certs/server.crt -a -f /keys/server.key ] || \
+create_pki_server
+
+for CN in ${PKI_CLIENT_CERTS}; do
+  [ -f /certs/client-${CN}.crt -a -f /keys/client-${CN}.key ] || \
+  create_pki_client ${CN}
+done
+
+cp /certs/client-*.crt /keys/client-*.key /keys-out/
+chmod go+r /keys-out/*
 
 echo "== Managing htpasswd for registry..."
 if [ -f /keys/registry_htpasswd ]; then
