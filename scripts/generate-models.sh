13d8ef4a4fbac593200ba56d8be2331898482c5d
Steffen Vogel <steffen.vogel@opal-rt.com>
2024-04-22 09:58:40 +0200

    nix: Fix generation of model and docs
    Signed-off-by: Steffen Vogel <steffen.vogel@opal-rt.com>

--- scripts/generate-models.sh
+++ scripts/generate-models.sh
@@ -4,7 +4,15 @@
 
 REPO=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )
 
+MODEL="${REPO}/seguro/commands/scheduler/compose_model.py"
+
+cat > "${MODEL}" <<EOF
+# SPDX-FileCopyrightText: 2023 Steffen Vogel, OPAL-RT Germany GmbH
+# SPDX-License-Identifier: Apache-2.0
+# flake8: noqa
+
+EOF
+
 poetry run datamodel-codegen  \
     --url https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json \
-    --input-file-type jsonschema \
-    --output "${REPO}/seguro/commands/scheduler/compose_model.py"
+    --input-file-type jsonschema >> "${MODEL}"
