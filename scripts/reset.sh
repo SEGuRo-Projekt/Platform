5d9805ff3dd76394df5eb31cd9ea4041fcdb53e7
Steffen Vogel <steffen.vogel@opal-rt.com>
2024-04-24 11:35:04 +0200

    Add a script to reset the platform to its factory state
    Signed-off-by: Steffen Vogel <steffen.vogel@opal-rt.com>

---
+++ scripts/reset.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# SPDX-FileCopyrightText: 2023 Steffen Vogel, OPAL-RT Germany GmbH
+# SPDX-License-Identifier: Apache-2.0
+
+read -p "Are you sure you want to reset the platform? Warning: all data will be erased! (y/n) " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	for PROJECT in scheduler platform; do
+		docker compose \
+			--project-name "${PROJECT}" \
+			down \
+				--remove-orphans \
+				--volumes 2>&1 |  \
+		grep -v "Warning: No resource found to remove"
+	done
+fi
