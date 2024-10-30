b43c17ce5d571da576894e7f35c068ff94fd532e
Felix Wege <fwege@eonerc.rwth-aachen.de>
2024-10-30 14:02:13 +0100

    Configure default and admin ACL
    Deny all events for default role and allow admin role to publish to all topics
    
    Signed-off-by: Felix Wege <fwege@eonerc.rwth-aachen.de>

--- scripts/setup-mosquitto.sh
+++ scripts/setup-mosquitto.sh
@@ -7,3 +7,10 @@ set -e
 rm -f /mosquitto/data/dynsec.json
 
 mosquitto_ctrl dynsec init /mosquitto/data/dynsec.json "${ADMIN_USERNAME}" "${ADMIN_PASSWORD}"
+
+mosquitto_ctrl ${OPTS} dynsec setDefaultACLAccess publishClientSend deny
+mosquitto_ctrl ${OPTS} dynsec setDefaultACLAccess publishClientReceive deny
+mosquitto_ctrl ${OPTS} dynsec setDefaultACLAccess subscribe deny
+mosquitto_ctrl ${OPTS} dynsec setDefaultACLAccess unsubscribe deny
+
+mosquitto_ctrl ${OPTS} dynsec addRoleACL admin publishClientSend '#' allow 5
