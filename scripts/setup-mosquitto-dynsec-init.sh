d41990bb9c8eea725236c61727b8ecb84ee2cf89
Felix Wege <fwege@eonerc.rwth-aachen.de>
2024-11-12 11:56:14 +0100

    Split mosquitto dynsec setup into two services
    Initialize dynsec config file before starting the broker service.
    
    Configuring DefaultACLAccess, as well as admin permission using
    mosquitto_ctrl using the running broker.
    
    Signed-off-by: Felix Wege <fwege@eonerc.rwth-aachen.de>

---
+++ scripts/setup-mosquitto-dynsec-init.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2086
+
+# SPDX-FileCopyrightText: 2023-2024 Steffen Vogel, OPAL-RT Germany GmbH
+# SPDX-License-Identifier: Apache-2.0
+
+set -e
+
+rm -f /mosquitto/data/dynsec.json
+
+mosquitto_ctrl dynsec init /mosquitto/data/dynsec.json "${ADMIN_USERNAME}" "${ADMIN_PASSWORD}"
