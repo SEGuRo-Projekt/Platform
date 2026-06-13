3acfe408b15a14995dbdbbf4826cdd74fd1db4dd
Henrik Hörter <henrik.hoerter@rwth-aachen.de>
2026-06-13 11:54:08 +0200

    chore: remove deadcode and printstatements
    Signed-off-by: Henrik Hörter <henrik.hoerter@rwth-aachen.de>

--- seguro/commands/s3_tool/main.py
+++ seguro/commands/s3_tool/main.py
@@ -9,13 +9,6 @@ from seguro.common import store
 from seguro.common import config
 
 
-def checkPath(path: str):
-    if Path(path).exists():
-        return
-    else:
-        raise FileNotFoundError(f"Local path not found: {path}")
-
-
 # push file/directory to S3 store
 def push(s: store.Client, args):
     remotepath = Path(args.remotefile)
@@ -27,7 +20,6 @@ def push(s: store.Client, args):
                     localpath.relative_to(localpath.parents[0])
                 )
             )
-            print(f"new remote {new_remote}")
         else:
             new_remote = args.remotefile
         push_one(s, localfile, new_remote)
@@ -47,7 +39,6 @@ def push_one(s: store.Client, localfile: str, remotefile: str):
                     str(remotepath.joinpath(element.relative_to(localpath)))
                     + "/"
                 )
-                print(f"remote file: {new_remote}")
             else:
                 new_remote = str(
                     remotepath.joinpath(element.relative_to(localpath))
@@ -59,8 +50,7 @@ def push_one(s: store.Client, localfile: str, remotefile: str):
 
 # pull file/directory from S3 store
 def pull(s: store.Client, args):
-    # TODO fix directory pull & check file suffix of local file
-    objects = list(  # get length of objects here? (objects is iterator)
+    objects = list(
         s.client.list_objects(  # get all objects in bucket with this prefix
             bucket_name=s.bucket, prefix=args.remotefile, recursive=True
         )
@@ -73,8 +63,6 @@ def pull(s: store.Client, args):
         remotepath = Path(args.remotefile)
         if localpath.is_dir():  # also covers the "." case
             args.localfile = str(localpath.joinpath(remotepath.name))
-        print(f"store objectname: {objects[0].object_name}")
-        print(f"local path: {args.localfile}")
         s.get_file(args.localfile, objects[0].object_name)
 
     else:  # pull directory
@@ -86,9 +74,6 @@ def pull(s: store.Client, args):
             raise NotADirectoryError(
                 f"Localpath is a file, not a directory {localbase}"
             )
-
-        print(f"local path: {args.localfile}")
-        print(f"remote base path: {args.remotefile}")
 
         for object in objects:  # get all objects
 
@@ -183,7 +168,7 @@ def list_elements(s: store.Client, args):
         recursive=False,
     )
 
-    for object in objects:  # get all objects
+    for object in objects:  # get all objects from Store
         element_in_dir = str(Path(object.object_name).relative_to(remotebase))
         if object.object_name.endswith("/"):
             element_in_dir += "/"
@@ -214,7 +199,6 @@ def main():
 
     subparsers = parser.add_subparsers(dest="command", required=True)
 
-    # push parser
     push_parser = subparsers.add_parser(
         "push", help="Push a local file to the storage"
     )
@@ -230,7 +214,6 @@ def main():
     )
     push_parser.set_defaults(func=push)
 
-    # pull parser
     pull_parser = subparsers.add_parser(
         "pull", help="Pull a local file from the storage"
     )
@@ -248,7 +231,6 @@ def main():
     )
     pull_parser.set_defaults(func=pull)
 
-    # get file
     get_file_parser = subparsers.add_parser(
         "get-file", help="Get a file from the storage"
     )
@@ -266,7 +248,6 @@ def main():
 
     get_file_parser.set_defaults(func=get_file)
 
-    # remove parser
     remove_parser = subparsers.add_parser(
         "remove", help="Remove a file/directory from the storage"
     )
@@ -275,7 +256,6 @@ def main():
     )
     remove_parser.set_defaults(func=remove)
 
-    # get file contents
     get_file_content_parser = subparsers.add_parser(
         "get-file-content", help="Get file content into terminal"
     )
@@ -286,7 +266,6 @@ def main():
 
     get_file_content_parser.set_defaults(func=get_file_content)
 
-    # put file contents
     put_file_content_parser = subparsers.add_parser(
         "put-file-content", help="Add filecontent as bytes into file in store"
     )
@@ -304,7 +283,6 @@ def main():
 
     put_file_content_parser.set_defaults(func=put_file_content)
 
-    # get file url
     get_file_url_parser = subparsers.add_parser(
         "get-file-url", help="Get File URL"
     )
@@ -315,7 +293,6 @@ def main():
 
     get_file_url_parser.set_defaults(func=get_file_url)
 
-    # put frame parser
     put_frame_parser = subparsers.add_parser(
         "put-frame",
         help="Upload a Pandas Dataframe as a Parquet file to the store",
@@ -337,7 +314,6 @@ def main():
 
     put_frame_parser.set_defaults(func=put_frame)
 
-    # get frame parser
     get_frame_parser = subparsers.add_parser(
         "get-frame",
         help="Download a Pandas Dataframe as a Parquet file from the store",
@@ -362,7 +338,7 @@ def main():
         "--path",
         type=str,
         help="Path of directory in store to list",
-        # default=".",
+        default=".",
     )
 
     list_parser.set_defaults(func=list_elements)
