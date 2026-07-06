d9e214183035d3aff5a2492ac450deeb62cd14f6
Henrik Hörter <henrik.hoerter@rwth-aachen.de>
2026-07-06 12:06:55 +0200

    fix(config): prevent side effects on other tools
    Signed-off-by: Henrik Hörter <henrik.hoerter@rwth-aachen.de>

--- seguro/common/config.py
+++ seguro/common/config.py
@@ -32,12 +32,12 @@ def resolve_from_project_root(p: str) -> str:
 
 
 # Authentication
-TLS_CACERT = resolve_from_project_root(env.str("TLS_CACERT", "keys/ca.crt"))
-TLS_CERT = resolve_from_project_root(
-    env.str("TLS_CERT", "keys/clients/admin.crt")
+TLS_CACERT = env.str("TLS_CACERT", resolve_from_project_root("keys/ca.crt"))
+TLS_CERT = env.str(
+    "TLS_CERT", resolve_from_project_root("keys/clients/admin.crt")
 )
-TLS_KEY = resolve_from_project_root(
-    env.str("TLS_KEY", "keys/clients/admin.key")
+TLS_KEY = env.str(
+    "TLS_KEY", resolve_from_project_root("keys/clients/admin.key")
 )
 
 # TLS_CACERT = env.str("TLS_CACERT", "keys/ca.crt")
